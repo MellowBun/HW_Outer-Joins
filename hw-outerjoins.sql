521d93d80865747af05ccccd79708fb35bf694a3
MellowBun <133154541+MellowBun@users.noreply.github.com>
2023-11-08 11:15:03 -0800

    Added file via upload

---
+++ hw-outerjoins.sql
@@ -0,0 +1,72 @@
+USE EntertainmentAgencyExample;
+
+-- *******************************************************************************
+-- 1. List the stage names of all entertainers who have had at least one engagement.
+-- Remove all duplicates. Do NOT use GROUP BY in your solution. (1 column, 12 rows)
+-- *******************************************************************************
+
+SELECT EntertainerID, EntStageName FROM Entertainers
+
+SELECT * FROM 
+
+
+
+-- *******************************************************************************
+-- 2. List the stage names of all entertainers and the IDs of any engagements they
+-- have booked. (2 columns, 112 rows)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 3. List the stage names of all entertainers who have never had an 
+-- engagement. (1 column, 1 row)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 4. List the stage names of all entertainers and the total number of engagements
+-- they have booked. Sort by total engagements from most to least. Hint: Make sure 
+-- your totals make sense compared to the results from #2 and #3. (2 columns, 13 rows)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 5. List the IDs of all musical styles and the IDs of customers who prefer those
+-- styles. Sort by style ID from lowest to highest. (2 columns, 41 rows)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 6. List the names of all musical styles and the first and last names of 
+-- customers who prefer those styles. Sort alphabetically by style name.
+-- (3 columns, 41 rows)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 7. List the names of all customers and the stage names of all entertainers they
+-- have booked. Include customers who have never booked an entertainer and 
+-- entertainers who have never been booked. Sort alphabetically by customer last
+-- name, then first name, then entertainer stage name. (3 columns, 114 rows)
+-- *******************************************************************************
+
+
+
+
+-- *******************************************************************************
+-- 8. List the names of all customers and the number of unique entertainers they
+-- have booked. Sort by total booked entertainers from most to least. Hint: Make 
+-- sure your totals make sense compared to your answer from #7. (3 columns, 15 rows)
+-- *******************************************************************************
+
+
