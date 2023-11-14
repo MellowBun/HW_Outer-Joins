0a2e5ec8862b4e7a345bb79905bb54ca9a4e9733
MellowBun <133154541+MellowBun@users.noreply.github.com>
2023-11-14 09:03:39 -0800

    Update hw-outerjoins.sql

--- hw-outerjoins.sql
+++ hw-outerjoins.sql
@@ -1,31 +1,31 @@
 USE EntertainmentAgencyExample;
+--HW: Outer Joins
+--HELP: 4, 5, , 
 
 -- *******************************************************************************
 -- 1. List the stage names of all entertainers who have had at least one engagement.
 -- Remove all duplicates. Do NOT use GROUP BY in your solution. (1 column, 12 rows)
 -- *******************************************************************************
 
-SELECT EntertainerID, EntStageName FROM Entertainers
-
-SELECT * FROM 
-
-
+--SELECT DISTINCT EntStageName FROM Entertainers
+--RIGHT JOIN Engagements ON Entertainers.EntertainerID = Engagements.EntertainerID
 
 -- *******************************************************************************
 -- 2. List the stage names of all entertainers and the IDs of any engagements they
 -- have booked. (2 columns, 112 rows)
 -- *******************************************************************************
 
-
-
+--SELECT EntStageName, EngagementNumber FROM Entertainers
+--LEFT JOIN Engagements ON Entertainers.EntertainerID = Engagements.EntertainerID
 
 -- *******************************************************************************
 -- 3. List the stage names of all entertainers who have never had an 
 -- engagement. (1 column, 1 row)
 -- *******************************************************************************
 
-
-
+--SELECT EntStageName FROM Entertainers
+--LEFT JOIN Engagements ON Entertainers.EntertainerID = Engagements.EntertainerID
+--WHERE Engagements.EntertainerID IS NULL
 
 -- *******************************************************************************
 -- 4. List the stage names of all entertainers and the total number of engagements
@@ -33,7 +33,11 @@ SELECT * FROM
 -- your totals make sense compared to the results from #2 and #3. (2 columns, 13 rows)
 -- *******************************************************************************
 
-
+--SELECT EntStageName, COUNT(*) AS NumOfEngagements FROM Entertainers
+--RIGHT JOIN Engagements ON Entertainers.EntertainerID = Engagements.EntertainerID
+--WHERE Engagements.EntertainerID IS NULL OR NOT NULL
+--GROUP BY EntStageName
+--SELECT * FROM Engagements
 
 
 -- *******************************************************************************
@@ -41,8 +45,8 @@ SELECT * FROM
 -- styles. Sort by style ID from lowest to highest. (2 columns, 41 rows)
 -- *******************************************************************************
 
-
-
+--SELECT CustomerID, StyleID FROM Musical_Preferences
+--ORDER BY StyleID ASC
 
 -- *******************************************************************************
 -- 6. List the names of all musical styles and the first and last names of 
@@ -50,8 +54,10 @@ SELECT * FROM
 -- (3 columns, 41 rows)
 -- *******************************************************************************
 
-
-
+--SELECT StyleName, CustFirstName, CustLastName FROM Musical_Styles
+--LEFT JOIN Musical_Preferences ON Musical_Styles.StyleID = Musical_Preferences.StyleID
+--LEFT JOIN Customers ON Musical_Preferences.CustomerID = Customers.CustomerID
+--ORDER BY StyleName ASC
 
 -- *******************************************************************************
 -- 7. List the names of all customers and the stage names of all entertainers they
@@ -59,7 +65,9 @@ SELECT * FROM
 -- entertainers who have never been booked. Sort alphabetically by customer last
 -- name, then first name, then entertainer stage name. (3 columns, 114 rows)
 -- *******************************************************************************
+--SELECT * FROM Entertainers
 
+--SELECT * FROM 
 
 
 
@@ -68,5 +76,4 @@ SELECT * FROM
 -- have booked. Sort by total booked entertainers from most to least. Hint: Make 
 -- sure your totals make sense compared to your answer from #7. (3 columns, 15 rows)
 -- *******************************************************************************
-
 
